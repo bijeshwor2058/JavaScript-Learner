2a6cae46beb9d2fbde33811804539a84759a251b
Bijeshwor Sharma <bijeshwor2058@gmail.com>
2025-01-01 19:34:30 +0530

    initial commit

---
+++ Day-03/Arrays and Methods.js
@@ -0,0 +1,25 @@
+/*----- Explicit type conversion -------- */
+
+// let num = 420;
+// let boolnumber = Boolean(num);
+// console.log(boolnumber);
+
+
+// let num = 0;
+// let boolnumber = Boolean(num);
+// console.log(boolnumber);
+
+
+/*---Loops---*/
+
+// let num =2;
+// let i = 1;
+// while(i<=10){
+//   console.log(num * i);
+//   i++;
+// }
+
+// let num = 2;
+// for(let i = 1; i<=10 ; i++){
+//   console.log(num * i)
+// }
